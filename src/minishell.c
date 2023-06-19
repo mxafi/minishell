e7a71b98ee47a803d5716ca0c7e483232f7a2b26
Markus Laaksonen <malaakso@student.hive.fi>
2023-06-19 11:57:53 +0300

    prep for merge to master

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 20:34:16 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/16 13:17:28 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/19 11:57:24 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,10 @@ void	display_prompt(void)
 	while (1)
 	{
 		input = get_input();
-		if (input)
-		{
-			// if (!*input)
-			// {
-				
-			// }
-		// 	lexer(input);
-			//ft_printf(">>>>%s<<<<\n", input);
-			free(input);
-		}
-	
+		if (!input)
+			return ;
+		lexer(input);
+		free(input);
 	}
 }
 
