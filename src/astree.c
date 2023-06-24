c273ed6595f73190894942074e94187bcd9b080a
Markus <95432159+mxafi@users.noreply.github.com>
2023-06-24 23:55:09 +0300

    shellfish grammar v1

--- src/astree.c
+++ src/astree.c
@@ -6,9 +6,29 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 12:33:28 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/24 21:54:07 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/24 23:54:46 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+/** shellfish grammar
+ * 
+ * <command_line>			::=	<command>
+ * 							|	<command_line> '|' <command>
+ * 
+ * <command>				::=	<simple_command>
+ * 							|	<simple_command> <redirector> <redirector_pipeline>
+ * 							|	<command> '<<' <argument>
+ * 
+ * <simple_command>			::=	<pathname>
+ * 							|	<simple_command> <argument>
+ * 
+ * <redirector>				::=	'<' | '>' | '>>'
+ * 
+ * <redirector_pipeline>	::=	<filename>
+ * 							|	<redirector> <redirector_pipeline>
+ * 
+ * <argument>				::=	ANY STRING TOKEN
+ * 
+ */
