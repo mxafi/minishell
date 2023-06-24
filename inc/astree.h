11c5f427794a4a24994f5602b608d82402751cdd
Markus Laaksonen <malaakso@student.hive.fi>
2023-06-24 13:36:40 +0300

    add defines and fix s_ast

--- inc/astree.h
+++ inc/astree.h
@@ -6,12 +6,14 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 12:32:41 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/24 12:23:46 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/24 13:18:03 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASTREE_H
 # define ASTREE_H
+
+# define AST_TOKEN_TYPE_UNDEFINED	-1
 
 // e_token_type from lexer.h for DEBUG
 enum e_token_list{
@@ -25,11 +27,11 @@ enum e_token_list{
 	HEREDOC,
 	APPEND_TO,
 	CMD
-} t_token_type;
+} e_token_type;
 
 typedef struct s_ast
 {
-	t_token_type	type;
+	int				type;
 	char			*data;
 	struct s_ast	*left;
 	struct s_ast	*right;
