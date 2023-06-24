8a45fa9216afea22c660c13d99dc18a16ac449db
Markus Laaksonen <malaakso@student.hive.fi>
2023-06-24 12:24:22 +0300

    add ast struct and token_type enum list

--- inc/astree.h
+++ inc/astree.h
@@ -6,13 +6,33 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 12:32:41 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/23 12:36:51 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/24 12:23:46 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASTREE_H
 # define ASTREE_H
 
+// e_token_type from lexer.h for DEBUG
+enum e_token_list{
+	SINGLE_QUOTE = '\'',
+	DOUBLE_QUOTE = '\"',
+	OUTFILE = '>',
+	INFILE = '<',
+	PIPE = '|',
+	SPACE,
+	STRING,
+	HEREDOC,
+	APPEND_TO,
+	CMD
+} t_token_type;
 
+typedef struct s_ast
+{
+	t_token_type	type;
+	char			*data;
+	struct s_ast	*left;
+	struct s_ast	*right;
+}					t_ast;
 
 #endif
