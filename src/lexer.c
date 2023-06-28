3edead54a6b5de98da0b34e04945836a0a8802a4
Lionel Clerc <lclerc@student.42.fr>
2023-06-29 00:27:34 +0300

    doxygened some functions and fixed few errors

--- src/lexer.c
+++ src/lexer.c
@@ -6,22 +6,23 @@
 /*   By: lionel <lionel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 15:26:38 by lclerc            #+#    #+#             */
-/*   Updated: 2023/06/26 18:29:20 by lionel           ###   ########.fr       */
+/*   Updated: 2023/06/29 00:27:34 by lionel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
 /**
- * @brief 
+ * @brief  CANT RECALL WHY I USED THIS instead of libft, to be checked
  * 
  * @param s 
- * @param start 
+ * @param input 
  * @param len 
  * @return char* 
  */
-char	*ft_test_substr(const char *s, unsigned int start, size_t len)
+char	*ft_test_substr(const char *s, unsigned int input, size_t len)
 {
+ // @brief  CANT RECALL WHY I USED THIS instead of libft, to be checked
 	char	*sub;
 	size_t	s_len;
 	size_t	max_sub_len;
@@ -30,9 +31,9 @@ char	*ft_test_substr(const char *s, unsigned int start, size_t len)
 	if (!s)
 		return (0);
 	s_len = ft_strlen(s);
-	if (start > s_len)
-		start = s_len;
-	max_sub_len = s_len - start;
+	if (input > s_len)
+		input = s_len;
+	max_sub_len = s_len - input;
 	if (len <= max_sub_len)
 		sub = malloc(len + 1);
 	else
@@ -42,7 +43,7 @@ char	*ft_test_substr(const char *s, unsigned int start, size_t len)
 	i = 0;
 	while (i < max_sub_len && i < len)
 	{
-		sub[i] = s[i + start];
+		sub[i] = s[i + input];
 		i++;
 	}
 	sub[i] = '\0';
@@ -51,13 +52,13 @@ char	*ft_test_substr(const char *s, unsigned int start, size_t len)
 }
 
 /**
- * @brief 
+ * @brief 	Add new token to the last node of the token list
  * 
- * @param list 
- * @param token 
- * @param str 
- * @param length 
- * @return int 
+ * @param list		token list being populated 
+ * @param token		new node to be added to the token list 
+ * @param str		input string being tokenized 
+ * @param length	length of the string to be tokenized 
+ * @return int		SUCCESS or FAILURE 
  */
 static int	tokenize_node(t_lexer *list, t_token *token, char *str, int length)
 {
@@ -68,7 +69,8 @@ static int	tokenize_node(t_lexer *list, t_token *token, char *str, int length)
 	ft_printf("token->token before substr is [] length [%d], str[%s]\n", length,
 			str);
 	ft_printf("strlen(str) [%d]\n", ft_strlen(str));
-	if (!(token->token = ft_test_substr(str, 0, length)))
+	// uses some test substr, see below, check!
+	if ((token->token = ft_test_substr(str, 0, length)) == FAILURE)
 	{
 		list->error_code = FAILURE;
 		return (FAILURE);
@@ -92,7 +94,15 @@ static int	tokenize_node(t_lexer *list, t_token *token, char *str, int length)
 	return (SUCCESS);
 }
 
-static int	string_to_token(t_lexer *token_list, char *start, char *delimiter)
+/**
+ * @brief Tokenize current string up to null terminator or delimiter
+ * 
+ * @param token_list	Token and list information placeholder
+ * @param input			string to be tokenized 
+ * @param delimiter		used to determine string length to be tokenized 
+ * @return int			SUCCESS or CALLOC_FAIL 
+ */
+static int	string_to_token(t_lexer *token_list, char *input, char *delimiter)
 {
 	t_token	*new_token;
 	int		length;
@@ -101,25 +111,34 @@ static int	string_to_token(t_lexer *token_list, char *start, char *delimiter)
 	new_token = NULL;
 	if (delimiter == NULL)
 	{
-		length = ft_strlen(start);
+		length = ft_strlen(input);
 	}
 	else
 	{
-		length = delimiter - start;
-		//	ft_printf("length [%d] = delimiter [%s] - start[%s]\n", length,
-		//			delimiter , start);
+		length = delimiter - input;
+		//	ft_printf("length [%d] = delimiter [%s] - input[%s]\n", length,
+		//			delimiter , input);
 	}
 	if (make_new_node(token_list, &new_token) == CALLOC_FAIL)
 		return (CALLOC_FAIL);
-	//ft_printf("length [%d] start [%s]\n", length, start);
-	if (!tokenize_node(token_list, new_token, start, length))
+	//ft_printf("length [%d] input [%s]\n", length, input);
+	if (!tokenize_node(token_list, new_token, input, length))
 		return (CALLOC_FAIL);
 	new_token->type = STRING;
-	start = delimiter;
+	input = delimiter;
 	return (SUCCESS);
 }
 
-static char	*delimiter_to_token(t_lexer *token_list, char *start)
+/**
+ * @brief	Tokenizes delimeters
+ * @details	The current char in input string is used as token type except for << and >>
+ * 			which require manual depiction due to the two characters identificatoion.
+ * 
+ * @param token_list  	Information and token list placeholder.
+ * @param input			Input string from readline being tokenized
+ * @return char*		Pointter to string after delimiter 
+ */
+static char	*delimiter_to_token(t_lexer *token_list, char *input)
 {
 	t_token	*new_token;
 	int		length;
@@ -127,10 +146,10 @@ static char	*delimiter_to_token(t_lexer *token_list, char *start)
 	new_token = NULL;
 	if (make_new_node(token_list, &new_token) == CALLOC_FAIL)
 		return (NULL);
-	if (ft_strncmp(start, "<<", 2) == 0 || ft_strncmp(start, ">>", 2) == 0)
+	if (ft_strncmp(input, "<<", 2) == 0 || ft_strncmp(input, ">>", 2) == 0)
 	{
 		length = 2;
-		if (*start == OUTFILE)
+		if (*input == OUTFILE)
 			new_token->type = APPEND_TO;
 		else
 			new_token->type = HEREDOC;
@@ -138,35 +157,44 @@ static char	*delimiter_to_token(t_lexer *token_list, char *start)
 	else
 	{
 		length = 1;
-		new_token->type = *start;
+		new_token->type = *input;
 	}
-	if (!tokenize_node(token_list, new_token, start, length))
+	if (!tokenize_node(token_list, new_token, input, length))
 		return (NULL);
-	return (start + length);
+	return (input + length);
 }
 
+/**
+ * @brief 	Readline string is tokenized using a self implemented version of strpbrk
+ * @details	The strpbrk seeks the input string for delimters: spaces, tabs, redirections
+ * 			pipes and quotes to output delimited strings a tokenized linked list. The 
+ * 			delimiter itself is then tokenized.
+ * 
+ * @param token_list	Token list and list information placeholder.
+ * @return int			Error code is initialized to 0 and return CALLOC_FAIL if it occurs
+ */
 static int	tokenize_readline(t_lexer *token_list)
 {
-	char	*start;
+	char	*input;
 	char	*delimiter;
 
 	delimiter = NULL;
-	start = token_list->readlined;
-	while (*start != '\0')
+	input = token_list->readlined;
+	while (*input != '\0')
 	{
-		delimiter = ft_strpbrk(start, DELIMITERS);
+		delimiter = ft_strpbrk(input, DELIMITERS);
 		//ft_printf("tokenize_readline -> ft_strpbrk gives delimeter: %s\n",
 			//	delimiter);
 		if (!delimiter)
 		{
-			string_to_token(token_list, start, NULL);
+			string_to_token(token_list, input, NULL);
 			break ;
 		}
 		else
 		{
-			if (start < delimiter)
-				string_to_token(token_list, start, delimiter);
-			start = delimiter_to_token(token_list, delimiter);
+			if (input < delimiter)
+				string_to_token(token_list, input, delimiter);
+			input = delimiter_to_token(token_list, delimiter);
 		}
 		if (token_list->error_code == CALLOC_FAIL)
 			break ;
@@ -174,6 +202,18 @@ static int	tokenize_readline(t_lexer *token_list)
 	return (token_list->error_code);
 }
 
+/**
+ * @brief 	The lexer prepares the input string from the shell prompt to be excuted
+ * @details	A lexer struct is initialized and serves as a placeholder for the tokenized
+ *			list.
+ *			- White spaces are trimmed from both ends of the readline input
+ * 			- Input is tokenized
+ *			- Syntax is validated
+ * 
+ * @param input String obrained by user via readline
+ * @return int	Return value is 0 if input was validated and execution was 
+ * 				acccomplished properly or an error code is returned.
+ */
 int	lexer(char *input)
 {
 	t_lexer	token_list;
@@ -182,13 +222,13 @@ int	lexer(char *input)
 	if (input)
 	{
 		token_list.readlined = ft_strtrim(input, WHITE_SPACES);
-		if ((validate_syntax(&token_list)) == SYNTAX_ERROR);
+		tokenize_readline(&token_list);
+		print_list(&token_list);
+		if ((validate_syntax(&token_list)) == SYNTAX_ERROR)
 		{
 			free_token_list(token_list);
 			return (SYNTAX_ERROR);
 		}
-		tokenize_readline(&token_list);
-		print_list(&token_list);
 	}
 	return (0);
 }
