af5ac3f122de63664100a98d15bb8f380c294b11
Markus Laaksonen <markus@mxa.fi>
2023-06-29 12:58:12 +0300

    fix variables to reflect changes in astree.h

--- src/astree_data_helpers.c
+++ src/astree_data_helpers.c
@@ -6,13 +6,13 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 21:51:55 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/28 20:08:46 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/29 12:57:24 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	ast_recursive_delete(t_ast *node)
+void	ast_recursive_delete(t_ast_node *node)
 {
 	if (!node)
 		return ;
@@ -23,7 +23,7 @@ void	ast_recursive_delete(t_ast *node)
 	free(node);
 }
 
-void	ast_set_exec_argv(t_ast *node, char *string, int position)
+void	ast_set_exec_argv(t_ast_node *node, char *string, int position)
 {
 	assert(node != NULL);
 	assert(node->exec_argv != NULL);
@@ -31,14 +31,14 @@ void	ast_set_exec_argv(t_ast *node, char *string, int position)
 	node->exec_argv[position] = string;
 }
 
-void	ast_set_type(t_ast *node, int token_type)
+void	ast_set_type(t_ast_node *node, t_ast_node_type type)
 {
 	assert(node != NULL);
-	assert(node->token_type == AST_TOKEN_TYPE_UNDEFINED);
-	node->token_type = token_type;
+	assert(node->type == AST_TOKEN_TYPE_UNDEFINED);
+	node->type = type;
 }
 
-void	ast_attach(t_ast *root, t_ast *left, t_ast *right)
+void	ast_attach(t_ast_node *root, t_ast_node *left, t_ast_node *right)
 {
 	assert(root != NULL);
 	root->left = left;
--- src/astree_data_helpers_alloc.c
+++ src/astree_data_helpers_alloc.c
@@ -6,22 +6,22 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 21:47:24 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/28 20:08:44 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/29 12:55:36 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
 // creates a node with optional data and type of AST_TOKEN_TYPE_UNDEFINED
-t_ast	*ast_create_node(char **exec_argv)
+t_ast_node	*ast_create_node(char **exec_argv)
 {
-	t_ast	*node;
+	t_ast_node	*node;
 
-	node = ft_calloc(1, sizeof(t_ast));
+	node = ft_calloc(1, sizeof(t_ast_node));
 	if (!node)
 		return (NULL);
 	node->exec_argv = exec_argv;
-	node->token_type = AST_TOKEN_TYPE_UNDEFINED;
+	node->type = AST_TOKEN_TYPE_UNDEFINED;
 	return (node);
 }
 
