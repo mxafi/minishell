d79cddec9f0ca8fbe663885f6aa8d64c5f3d6243
Markus Laaksonen <markus@mxa.fi>
2023-06-29 12:53:55 +0300

    restructure astree.h

--- inc/astree.h
+++ inc/astree.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 12:32:41 by malaakso          #+#    #+#             */
-/*   Updated: 2023/06/28 19:54:07 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/06/29 12:53:21 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # define AST_TOKEN_TYPE_UNDEFINED	-1
 
 // e_token_type from lexer.h for DEBUG
-enum e_token_list{
+typedef enum e_token_list{
 	SINGLE_QUOTE,
 	DOUBLE_QUOTE,
 	OUTFILE,
@@ -30,24 +30,45 @@ enum e_token_list{
 	BI_CMD,
 	REDIR,
 	ARG
-} e_token_type;
+}	t_token_type;
 
 // expected input for ast_builder from lexer, here for debug purposes
 typedef struct s_token
 {
-	int				type;
+	t_token_type	type;
 	char			*data;
 	struct s_token	*next;
 }					t_token;
 
-typedef struct s_ast
+typedef enum e_ast_node_type
 {
-	int				token_type;
+	AST_PIPE,
+	AST_COMMAND
+}	t_ast_node_type;
+
+typedef enum e_ast_redir_type
+{
+	AST_INFILE,
+	AST_OUTFILE,
+	AST_APPEND
+}	t_ast_redir_type;
+
+typedef struct s_redir
+{
+	t_ast_redir_type	type;
+	int					file_descriptor;
+	char				*argument;
+}						t_redir;
+
+typedef struct s_ast_node
+{
+	t_ast_node_type	type;
+	int				argv_count;
+	int				redir_count;
 	char			**exec_argv;
+	t_redir			**redirections;
 	struct s_ast	*left;
 	struct s_ast	*right;
-}					t_ast;
-
-
+}					t_ast_node;
 
 #endif
