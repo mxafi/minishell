9a8a3743130583e594941a0ee6cb8dd2978b043f
Markus Laaksonen <markus@mxa.fi>
2023-08-24 09:35:46 +0300

    norm refactor env.c (env_utils.c)

--- inc/env.h
+++ inc/env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lclerc <lclerc@hive.student.fi>            +#+  +:+       +#+        */
+/*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 12:50:39 by malaakso          #+#    #+#             */
-/*   Updated: 2023/08/16 11:28:13 by lclerc           ###   ########.fr       */
+/*   Updated: 2023/08/24 09:35:05 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,9 @@ int			init_envp(void);
 void		env_print_list(void);
 void		env_unset_key(char *key);
 void		env_set_value_by_key(char *key, char *value);
+char		*env_split_value(char *env_var);
+char		*env_utils_join_three_str(char *s1, char *s2, char *s3);
+size_t		env_utils_ret_key_idx(char *key);
+void		envp_init_vars(size_t i);
+
 #endif
