534d4c34440803589bfd2ae7dbab3c26ce1f8bf6
Lionel Clerc <lionel.clerc@protonmail.com>
2023-08-24 11:36:17 +0300

    Norminetted lexer_label_toke n_types.c

--- src/lexer_label_token_type.c
+++ src/lexer_label_token_type.c
@@ -6,12 +6,11 @@
 /*   By: lclerc <lclerc@hive.student.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 17:38:05 by lclerc            #+#    #+#             */
-/*   Updated: 2023/08/15 16:14:22 by lclerc           ###   ########.fr       */
+/*   Updated: 2023/08/24 11:35:10 by lclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-
 
 /**
  * @brief			Labels token types and token list's state 
@@ -21,8 +20,10 @@
  * 
  * @param list 		Head of token list and information placeholder
  * @param token		Current token 
- * @param state		Current state of the token list (UNDEFINED, QUOTE_OPENED_ QUOTE_CAN_BE_CLOSED) 
- * @param type		Type of token as obtained from ft_strpbrk as QUOTE_NEED_NULL_STR 
+ * @param state		Current state of the token list (UNDEFINED, QUOTE_OPENED_
+ * 					QUOTE_CAN_BE_CLOSED) 
+ * @param type		Type of token as obtained from ft_strpbrk as 
+ * 					QUOTE_NEED_NULL_STR 
  */
 static void	set_token_type_and_list_state(t_lexer *list, t_token *token,
 		t_token_type type, t_validation_state state)
@@ -33,6 +34,17 @@ static void	set_token_type_and_list_state(t_lexer *list, t_token *token,
 		token->content = NULL;
 }
 
+static void	handle_list_state_is_undefined(t_lexer *list, t_token *token,
+		char *input)
+{
+	if (list->state == UNDEFINED && input[0] == '\'')
+		set_token_type_and_list_state(list, token, SINGLE_QUOTE,
+			SGL_QUOTE_OPENED);
+	else if (list->state == UNDEFINED && input[0] == '\"')
+		set_token_type_and_list_state(list, token, DOUBLE_QUOTE,
+			DBL_QUOTE_OPENED);
+}
+
 /**
  * @brief		Labels token types and the current state of the token list  
  * @details		Helper function labelling token types, and handles quotes
@@ -40,7 +52,8 @@ static void	set_token_type_and_list_state(t_lexer *list, t_token *token,
  *				is changed to (..)QUOTE_OPENED status. 
  *				Any token type being handled while the token list is in opened
  *				quote mode will be labelled as a SGL or DBL_QUOTE_STR.
- *				If the list does not end with an UNDEFINED state, syntax validation
+ *				If the list does not end with an UNDEFINED state,
+					syntax validation
  * 				will rejected the input. See lexer_quote_validation.c
  * 
  * @param list	Head and placeholder for information of the token list 
@@ -49,32 +62,30 @@ static void	set_token_type_and_list_state(t_lexer *list, t_token *token,
  */
 static void	handle_quotes(t_lexer *list, t_token *token, char *input)
 {
-	if (list->state == UNDEFINED && input[0] == '\'')
-		set_token_type_and_list_state(list, token, SINGLE_QUOTE, \
-				SGL_QUOTE_OPENED);
-	else if (list->state == UNDEFINED && input[0] == '\"')
-		set_token_type_and_list_state(list, token, DOUBLE_QUOTE, \
-				DBL_QUOTE_OPENED);
+	if (list->state == UNDEFINED)
+		handle_list_state_is_undefined(list, token, input);
 	else if (list->state == SGL_QUOTE_OPENED && input[0] != '\'')
-		set_token_type_and_list_state(list, token, SGL_QUOTE_STR, \
-				SGL_QUOTE_CAN_BE_CLOSED);
+		set_token_type_and_list_state(list, token, SGL_QUOTE_STR,
+			SGL_QUOTE_CAN_BE_CLOSED);
 	else if (list->state == DBL_QUOTE_OPENED && input[0] != '\"')
-		set_token_type_and_list_state(list, token, DBL_QUOTE_STR, \
-				DBL_QUOTE_CAN_BE_CLOSED);
+		set_token_type_and_list_state(list, token, DBL_QUOTE_STR,
+			DBL_QUOTE_CAN_BE_CLOSED);
 	else if (list->state == SGL_QUOTE_CAN_BE_CLOSED && input[0] == '\'')
 		set_token_type_and_list_state(list, token, SINGLE_QUOTE, UNDEFINED);
 	else if (list->state == DBL_QUOTE_CAN_BE_CLOSED && input[0] == '\"')
 		set_token_type_and_list_state(list, token, DOUBLE_QUOTE, UNDEFINED);
 	else if (list->state == SGL_QUOTE_CAN_BE_CLOSED && input[0] != '\'')
-		set_token_type_and_list_state(list, token, SGL_QUOTE_STR, SGL_QUOTE_CAN_BE_CLOSED);
+		set_token_type_and_list_state(list, token, SGL_QUOTE_STR,
+			SGL_QUOTE_CAN_BE_CLOSED);
 	else if (list->state == DBL_QUOTE_CAN_BE_CLOSED && input[0] != '\"')
-		set_token_type_and_list_state(list, token, DBL_QUOTE_STR, DBL_QUOTE_CAN_BE_CLOSED);
+		set_token_type_and_list_state(list, token, DBL_QUOTE_STR,
+			DBL_QUOTE_CAN_BE_CLOSED);
 	else if (list->state == SGL_QUOTE_OPENED && input[0] == '\'')
-		set_token_type_and_list_state(list, token, QUOTE_NEED_NULL_STR, \
-				UNDEFINED);
+		set_token_type_and_list_state(list, token, QUOTE_NEED_NULL_STR,
+			UNDEFINED);
 	else if (list->state == DBL_QUOTE_OPENED && input[0] == '\"')
-		set_token_type_and_list_state(list, token, QUOTE_NEED_NULL_STR, \
-				UNDEFINED);
+		set_token_type_and_list_state(list, token, QUOTE_NEED_NULL_STR,
+			UNDEFINED);
 }
 
 /**
@@ -84,13 +95,14 @@ static void	handle_quotes(t_lexer *list, t_token *token, char *input)
  * 			if the following token is to be labelled as found by ft_strpbrk()
  * 			or if it is a quote type of string SGL/DBL_QUOTE_STR.
  *
- * @param list			Head of the token list and placeholder for list information
+ * @param list			Head of the token list and placeholder for list 
+ * information
  * @param token 		The current token being treated
  * @param token_type	The type of token 
  * @param input			The input string currently being handled 
  */
-void	label_token_type(t_lexer *list, t_token *token, \
-				t_token_type token_type, char *input)
+void	label_token_type(t_lexer *list, t_token *token, t_token_type token_type,
+		char *input)
 {
 	if (list->state == UNDEFINED)
 	{
@@ -110,10 +122,8 @@ void	label_token_type(t_lexer *list, t_token *token, \
 				token->type = MY_SPACE;
 		}
 	}
-	else if (list->state == SGL_QUOTE_OPENED || list->state == DBL_QUOTE_OPENED \
-		|| list->state == SGL_QUOTE_CAN_BE_CLOSED || \
-		list->state == DBL_QUOTE_CAN_BE_CLOSED)
+	else if (list->state == SGL_QUOTE_OPENED || list->state == DBL_QUOTE_OPENED
+		|| list->state == SGL_QUOTE_CAN_BE_CLOSED
+		|| list->state == DBL_QUOTE_CAN_BE_CLOSED)
 		handle_quotes(list, token, input);
-	else
-		printf("If you read this, the token type was not handled properly in label_token_type()"); // delete me 
 }
