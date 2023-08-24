c558a1eb061f394f59329792867cef7c84fa7737
Lionel Clerc <lionel.clerc@protonmail.com>
2023-08-24 14:45:50 +0300

    doxygen lexer_expansion_extra.c functions()

--- src/lexer_expansion_extra.c
+++ src/lexer_expansion_extra.c
@@ -6,7 +6,7 @@
 /*   By: lclerc <lclerc@hive.student.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 14:33:51 by lclerc            #+#    #+#             */
-/*   Updated: 2023/08/24 14:42:04 by lclerc           ###   ########.fr       */
+/*   Updated: 2023/08/24 14:45:14 by lclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,19 @@ static void	handle_exit_status(const char **env_value, t_lexer *list,
 	*env_value = exit_status_value;
 }
 
+/**
+ * @brief Handles environment variable expansion based on key value.
+ *
+ * @details This function handles environment variable expansion by evaluating
+ * the given key value. If the key value is "$", it returns a string containing
+ * "$" to represent a literal dollar sign. If the key value is "?", it handles
+ * the exit status and updates the env_value accordingly. Otherwise, it looks
+ * up the environment value by the key and returns it.
+ *
+ * @param key_value The key value extracted from the environment variable key.
+ * @param list The lexer list containing context information.
+ * @return A pointer to the resulting environment value.
+ */
 const char	*handle_expansion(char *key_value, t_lexer *list)
 {
 	const char	*env_value;
@@ -47,7 +60,14 @@ const char	*handle_expansion(char *key_value, t_lexer *list)
 	return (env_value);
 }
 
-
+/**
+ * @brief Retrieves the substring of input_string before the dollar sign.
+ *
+ * @param dollar_sign The position of the dollar sign.
+ * @param input_string The input string.
+ * @param result_string The result string before the dollar sign.
+ * @return A pointer to the extracted substring.
+ */
 static char	*get_string_before_dollar(char *dollar_sign, char *input_string,
 		char *result_string)
 {
@@ -62,7 +82,15 @@ static char	*get_string_before_dollar(char *dollar_sign, char *input_string,
 	return (tmp);
 }
 
-
+/**
+ * @brief Constructs the result string before the dollar sign.
+ *
+ * @param input The input string.
+ * @param dollar_sign The position of the dollar sign.
+ * @param result_string The existing result string.
+ * @param handled_pre_string Flag indicating if a string before dollar sign is handled.
+ * @return A pointer to the constructed result string.
+ */
 char	*get_result_string(char *input, char *dollar_sign,
 			char *result_string, t_bool handled_pre_string)
 {
